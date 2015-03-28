8ed73b5f6980ac1d685ec1c0e4ad23c54dd9c958
Christopher Walmsley <chris@harvestyield.com>
2015-03-28 10:16:25 -0400

    Initial Commit

---
+++ WindMeterSDK-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WindMeterSDK-Swift
+//
+//  Created by Christopher Walmsley on 3/28/15.
+//  Copyright (c) 2015 Christopher Walmsley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
