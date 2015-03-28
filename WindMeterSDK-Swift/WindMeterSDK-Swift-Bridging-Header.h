91b276daa0cebe864c99879aa4988016a82504c9
Christopher Walmsley <chris@harvestyield.com>
2015-03-28 11:24:42 -0400

    First Commit

---
+++ WindMeterSDK-Swift/WindMeterSDK-Swift-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  WindMeterSDK-Swift-Bridging-Header.h
+//  WindMeterSDK-Swift
+//
+//  Created by Christopher Walmsley on 3/28/15.
+//  Copyright (c) 2015 Christopher Walmsley. All rights reserved.
+//
+
+#ifndef WindMeterSDK_Swift_WindMeterSDK_Swift_Bridging_Header_h
+#define WindMeterSDK_Swift_WindMeterSDK_Swift_Bridging_Header_h
+
+
+#endif
